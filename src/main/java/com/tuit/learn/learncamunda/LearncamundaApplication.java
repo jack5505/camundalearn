1d108c72d6bef6b4d8b57633dd8cd1bc50c12b69
jack <jahon.080879@gmail.com>
2025-03-20 12:48:59 +0500

    first commit

---
+++ src/main/java/com/tuit/learn/learncamunda/LearncamundaApplication.java
@@ -0,0 +1,13 @@
+package com.tuit.learn.learncamunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearncamundaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearncamundaApplication.class, args);
+    }
+
+}
